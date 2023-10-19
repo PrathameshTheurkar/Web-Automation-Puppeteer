6410f22ea9ce8e9c58cb5e4db4db664a0388b8e9
Prathamesh <prathameshtheurkar037@gmail.com>
2023-10-19 19:43:22 +0530

    Updated

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ async function func(){
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({headless : false});
   const page = await browser.newPage();
+  
 
   // Navigate the page to a URL
   await page.goto('https://en.wikipedia.org/wiki/Coronavirus');
-
+  
+  await page.click('#vector-main-menu-dropdown-checkbox')
   await page.screenshot({path : 'wiki.png'});
+  const result = await page.evaluate(()=>{
+    let a = document.querySelectorAll('.mw-headline')
+    let b = [...a]
+
+    return b.map(h => h.innerText)
+  })
+  console.log(result)
+
+  
   await browser.waitForTarget(()=>false)
   await browser.close();
 }
